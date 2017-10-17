bc72aed08786d0ad115bf206c413dc769c1be645
Satyam Pathak <teawithsatyam@gmail.com>
2017-10-17 14:31:45 +0630

    first commit

---
+++ card.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Card</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+</head>
+<body>
+	<div class="row">
+	<div class="col m5 s12 offset-m3">
+	<div class="col s12 m12">
+    
+    <div class="card horizontal">
+      <div class="card-image valign-wrapper">
+        <img style="margin: 2px;" src="user.png">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content" style="padding: 0">
+        	<div class="col m9 s12 valign-wrapper">
+		      <ul id="nav-mobile" class="left">
+		        <li><h5>Satyam Pathak</h5></li>
+		        <li><p>Computers, WD Delhi</p></li>
+		      </ul></div>
+
+		    
+		      <div class="col m3 " style="position: absolute;/* left: 0; */right: 0;height: 100%;/* height: 101px; */background: #0db129;">
+		     	 <ul id="nav-mobile" class="" style="
+					    text-align: center;
+					    color: white;
+					    /* margin: auto; */
+					">
+		        <li><p><b>Physics</p></b></li>
+		        <li><p>IIT DELHI</p></li>
+		        <li><img class="responsive-img hoverable" src="play-button (1).png"></li>
+
+
+		      </ul>
+		      
+		     </div>
+
+        </div>
+       
+      </div>
+    </div>
+  </div>
+ </div>
+</div>
+</body>
+</html>
